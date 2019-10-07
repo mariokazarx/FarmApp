ef5464a8ff12cc8ac6a065fa9fe033395bf7d42a
mariojurado@hotmail.es <jurado88>
2019-10-06 20:23:05 -0500

    primer comentario

---
+++ app/src/main/java/com/example/farmapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.farmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button infoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        infoButton = findViewById(R.id.btn_info);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(MainActivity.this, InfoActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+}
