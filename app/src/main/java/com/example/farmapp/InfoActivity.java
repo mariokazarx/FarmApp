0212f3a5c5ed9ccbf09fed06811ca782a2d94aaa
maribel <maribelperez2304@hotmail.com>
2019-10-09 22:47:59 -0500

    Actualización de diseño y tema

--- app/src/main/java/com/example/farmapp/InfoActivity.java
+++ app/src/main/java/com/example/farmapp/InfoActivity.java
@@ -16,8 +16,6 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
